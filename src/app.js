41b04e7d55166cb76bce8601107d942039503eb8
JuanjoMend1 <jmendez@digyto.mx>
2023-08-07 22:10:25 -0600

    Se agregan cambios

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.get('/token', (req, res) => {
 });
 
 app.post('/auth', bodyParser(), async (req, res) => {
+    console.log("entro")
     try {
         console.log( Date().toString() + "GET /auth");
         const response = await auth.accessToken(req.body.code);
@@ -51,5 +52,56 @@ app.get('/refresh', async (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Listening on http://localhost:3000/");
+    console.log("Listening on https://mendezj.site/");
 })
+
+
+// const express = require('express');
+// const jwt = require('jsonwebtoken');
+// const axios = require('axios');
+
+// const app = express();
+// app.use(express.json());  // para poder parsear el cuerpo JSON
+
+// // Apple publica sus claves públicas aquí:
+// const APPLE_KEYS_URL = "https://appleid.apple.com/auth/keys";
+
+// app.post('/validate-apple-token', async (req, res) => {
+//     const token = req.body.token;
+
+//     try {
+//         const appleKeysResponse = await axios.get(APPLE_KEYS_URL);
+//         const keys = appleKeysResponse.data.keys;
+//         let appleKey;
+
+//         for (let key of keys) {
+//             try {
+//                 appleKey = key;
+//                 jwt.verify(token, getPublicKey(appleKey), {
+//                     algorithms: appleKey.alg,
+//                     issuer: 'https://appleid.apple.com',
+//                     audience: 'io.digybot.appuat',  // tu Apple Client ID
+//                 });
+
+//                 // Si no hay errores en la verificación, el token es válido
+//                 return res.status(200).send("Token válido");
+//             } catch (error) {
+//                 // Ignora el error y prueba con la siguiente clave
+//             }
+//         }
+
+//         res.status(401).send("Token inválido");
+//     } catch (err) {
+//         res.status(500).send("Error al validar el token");
+//     }
+// });
+
+// function getPublicKey(key) {
+//     return `-----BEGIN PUBLIC KEY-----\n${key.n}\n-----END PUBLIC KEY-----`;
+// }
+
+// app.listen(3000, () => {
+//     console.log('Servidor escuchando en el puerto 3000');
+// });
+
+
